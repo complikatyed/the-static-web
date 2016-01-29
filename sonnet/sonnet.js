c224f5dede05d2b70c05890ef110c631ade9f84d
Complikatyed <katye.russell@gmail.com>
2016-01-29 17:08:07 -0600

    Find total number of characters in the sonnet

--- sonnet/sonnet.js
+++ sonnet/sonnet.js
@@ -1,4 +1,6 @@
 var text = document.getElementById("sonnet").innerHTML;
 console.log( text);
 
-console.log("Orphans begins at ", text.indexOf("orphans"));
+console.log("Orphans begins at", text.indexOf("orphans"));
+
+console.log("Total characters in the sonnet:", text.length);
