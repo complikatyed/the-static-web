4dcf4abedfdd59288445b7636317a890e12ebdad
Complikatyed <katye.russell@gmail.com>
2016-01-29 17:17:22 -0600

    Replace first instance of 'winter' with 'yuletide'

--- sonnet/sonnet.js
+++ sonnet/sonnet.js
@@ -4,3 +4,7 @@ console.log( text);
 console.log("Orphans begins at", text.indexOf("orphans"));
 
 console.log("Total characters in the sonnet:", text.length);
+
+var firstNewSonnet = text.replace("winter", "yuletide");
+console.log(firstNewSonnet);
+document.getElementById("sonnet").innerHTML = firstNewSonnet;
