596372e30288b1f25a2853d945a5b33a6c399052
Complikatyed <katye.russell@gmail.com>
2016-01-29 20:02:50 -0600

    Add flawed array method

--- reindeer/reindeer.js
+++ reindeer/reindeer.js
@@ -2,4 +2,17 @@ var colors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwin
 var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
 
 
-var hohohoElement = document.getElementById("coloredReindeer");
+
+
+// Loop through the reindeer and add the name of the reindeer to the div.
+// Prepend the name of the reindeer with the corresponding color from the other array.
+
+var hohohoElement = [];
+for(i=0; i < reindeer.length; i++) {
+  hohohoElement.push("<p>" + colors[i] + " " + reindeer[i] + "</p>");
+}
+console.log(hohohoElement);
+
+document.getElementById("coloredReindeer").innerHTML = hohohoElement;
+
+
