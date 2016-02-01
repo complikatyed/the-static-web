b1ba3d6d549df180a477f39272a96f5c50911d4d
Complikatyed <katye.russell@gmail.com>
2016-02-01 10:35:59 -0600

    Coins calcuating but not in object or function

--- JS/coins/coins.js
+++ JS/coins/coins.js
@@ -10,14 +10,41 @@
 
 */
 
-function coinCounter () {
-  // Initialize a JavaScript object to hold the coins
-  var coinPurse = {};
+// function coinCounter () {
+//   // Initialize a JavaScript object to hold the coins
+//   var coinPurse = {};
 
-  coinPurse.quarters = 0;
+//   coinPurse.quarters = 0;
 
-  return coinPurse;
+//   return coinPurse;
+// }
+
+// var coins = coinCounter()
+// console.log();
+
+var amount = 2.74;
+var quarters = 0;
+var dimes = 0;
+var nickels = 0;
+var pennies = 0;
+
+
+amount *= 100;
+while (amount >= 25) {
+  quarters += 1;
+  amount -= 25;
+} while (amount >= 10) {
+  dimes += 1;
+  amount -= 10;
+} while (amount >= 5) {
+  nickels += 1;
+  amount -= 5;
+} while (amount >= 1) {
+  pennies += 1;
+  amount -= 1;
 }
 
-var coins = coinCounter()
-console.log();
+console.log("quarters:", quarters);
+console.log("dimes:", dimes);
+console.log("nickels:", nickels);
+console.log("pennies:", pennies);
